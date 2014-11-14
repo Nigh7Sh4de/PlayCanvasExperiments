344bf44e8dfd7c9b8b13825cc554a72a75ef4ff8
Nigh7Sh4de <plotnikd@gmail.com>
2014-11-14 16:56:35 -0500

    Spinning cube complete
    Achivement Unlocked: Not Completely Inept!

---
+++ Experiments/SpinningCube/app.js
@@ -0,0 +1,49 @@
+// Create a PlayCanvas application
+var canvas = document.getElementById("application-canvas");
+var app = new pc.fw.Application(canvas, {});
+app.start();
+
+// Fill the available space at full resolution
+app.setCanvasFillMode(pc.fw.FillMode.FILL_WINDOW);
+app.setCanvasResolution(pc.fw.ResolutionMode.AUTO);
+
+// Create box entity
+var e = new pc.fw.Entity();
+app.context.systems.model.addComponent(e, {
+type: "box",
+});
+
+var material = new pc.scene.PhongMaterial();
+material.diffuse = new pc.Color(1.0, 1.0, 0.0, 1.0);
+material.update();
+
+e.model.material = material;
+
+
+// Create camera entity
+var cam = new pc.fw.Entity();
+	app.context.systems.camera.addComponent(cam, {
+clearColor: [0.1,0.1,0.1]
+});
+
+// Create directional light entity
+var light = new pc.fw.Entity();
+	app.context.systems.light.addComponent(light, {
+color: new pc.Color(1,1,1)
+});
+
+// Add to hierarchy
+app.context.root.addChild(e);
+app.context.root.addChild(cam);
+app.context.root.addChild(light);
+
+// Set up position and orientation
+cam.setLocalPosition(0, 0, 3);
+light.setEulerAngles(45, 0, 0);
+
+// Register an update event
+app.on("update", function (dt) {
+	
+	e.rotate(30*dt, 45*dt, 20*dt);
+
+});
