52842f3976fb7565f4e15cb46ff1cbd651a3c1b8
Nigh7Sh4de <plotnikd@gmail.com>
2014-11-16 13:36:43 -0500

    Added a skybox to travelling cube

--- Experiments/TravellingCube/app.js
+++ Experiments/TravellingCube/app.js
@@ -11,7 +11,51 @@ app.setCanvasResolution(pc.fw.ResolutionMode.AUTO);
 
 app.context.scene.ambientLight = new pc.Color(0.2, 0.2, 0.2);
 
-// Create box entity
+//Create skybox entity
+var skybox = new pc.fw.Entity();
+
+var up = new pc.asset.Asset('up', "texture", {
+	url: 'assets/space_up.png'
+});
+var down = new pc.asset.Asset('down', "texture", {
+	url: 'assets/space_down.png'
+});
+var left = new pc.asset.Asset('left', "texture", {
+	url: 'assets/space_left.png'
+});
+var right = new pc.asset.Asset('right', "texture", {
+	url: 'assets/space_right.png'
+});
+var back = new pc.asset.Asset('back', "texture", {
+	url: 'assets/space_back.png'
+});
+var front = new pc.asset.Asset('front', "texture", {
+	url: 'assets/space_front.png'
+});
+
+var hex = new pc.asset.Asset('hex', "texture", {
+	url: 'assets/Hex_Plating_s.png'
+});
+
+
+app.context.assets.addAsset(up);
+app.context.assets.addAsset(down);
+app.context.assets.addAsset(left);
+app.context.assets.addAsset(right);
+app.context.assets.addAsset(front);
+app.context.assets.addAsset(back);
+
+app.context.systems.skybox.addComponent(skybox, {
+	enabled: true,
+	posx: right.id,
+	negx: left.id,
+	posy: up.id,
+	negy: down.id,
+	posz: front.id,
+	negz: back.id
+});	
+
+//Create box entity
 var box = new pc.fw.Entity();
 app.context.systems.model.addComponent(box, {
 	type: "box",
@@ -78,5 +122,19 @@ app.on("update", function (dt) {
 	}
 	if (keyboard.isPressed(pc.input.KEY_D)) {
 		box.translate(+2*dt,0,0);
+	}
+
+
+	if (keyboard.isPressed(pc.input.KEY_LEFT)) {
+		cam.rotate(0, 100*dt, 0);
+	}
+	if (keyboard.isPressed(pc.input.KEY_RIGHT)) {
+		cam.rotate(0, -100*dt, 0);
+	}
+	if (keyboard.isPressed(pc.input.KEY_UP)) {
+		cam.rotate(-100*dt, 0, 0);
+	}
+	if (keyboard.isPressed(pc.input.KEY_DOWN)) {
+		cam.rotate(100*dt, 0, 0);
 	}
 });
